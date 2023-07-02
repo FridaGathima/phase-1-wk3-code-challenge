672f836b207c61d690618a942aed13d43a9bdc57
+ <+>
2023-07-03 00:48:39 +0300

    wk3_code_challenge

---
+++ index.js
@@ -0,0 +1,170 @@
+
+const movies = document.getElementById('films')
+fetch("http://localhost:3000/films")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(films => {
+            console.log (films)
+
+            const moviesDetails = document.createElement('li')
+            moviesDetails.innerText = films.title
+            movies.appendChild(moviesDetails)
+            
+            }
+          ) 
+     })
+
+
+function displayFirstMovie() {
+    fetch('http://localhost:3000/films/1')
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data)
+
+            const firstMovieCont = document.getElementById('firstmoviecontainer')
+
+            const movieHead = document.querySelector('h4')
+            movieHead.innerText = data.title
+            firstMovieCont.appendChild(movieHead)
+
+            const moviePoster = document.getElementById('poster')
+            moviePoster.innerHTML = `<img src = "${data.poster}" alt= "The Giant Gila Monster">`
+            firstMovieCont.appendChild(moviePoster)
+
+            const movieRuntime = document.getElementById('runtime')
+            movieRuntime.innerText = 'The runtime for this movie is ' + '' + data.runtime + '' + ' minutes'
+            firstMovieCont.appendChild(movieRuntime)
+
+            const movieShowtime = document.getElementById('showtime')
+            movieShowtime.innerText = 'This movie starts at ' + '' + data.showtime
+            firstMovieCont.appendChild(movieShowtime)
+
+            const ticketbutton = document.createElement('button')
+            ticketbutton.id = 'availableticketss'
+            ticketbutton.innerText = 'AVAILABLE TICKETS'
+            firstMovieCont.appendChild(ticketbutton)
+
+            const tickets = document.getElementById('availabletickets')
+            tickets.innerText = 'available_tickets'
+            firstMovieCont.appendChild(tickets)
+
+            const ticketting = document.getElementById('availabletickets')
+            ticketting.addEventListener('click', availableTickets)
+
+
+            // tickets.addEventListener('click', function availableTickets(ticketssold, theatrecapacity) {
+            //     if (ticketssold <= theatrecapacity) {
+            //         if (ticketssold === theatrecapacity) {
+            //             return 'no available tickets'
+            //         } else {
+            //             if (ticketssold < theatrecapacity) {
+            //                 return (theatrecapacity - ticketssold)
+            //             }
+            //         }
+            //     }
+            // })
+
+            //tickets.innerText = availableTickets (ticketssold, theatrecapacity)
+            //const availabletickets = availableTickets (ticketssold, theatrecapacity)
+            
+            
+        })
+}
+displayFirstMovie()
+
+
+function availableTickets(ticketssold, theatrecapacity) {
+    if (ticketssold <= theatrecapacity) {
+        if (ticketssold === theatrecapacity) {
+            return 'no available tickets'
+        } else {
+            if (ticketssold < theatrecapacity) {
+                return (theatrecapacity - ticketssold)
+            }
+        }
+    }
+}
+
+
+
+
+
+// movieHead.addEventListener ('click', ()=> {
+//     availableTickets(ticketssold, theatrecapacity)      
+// })
+
+
+ //     const firstMovie = document.createElement('div')
+    //     firstMovie.innerHTML = `
+    //     ${data.title}
+    //     <img src = "${data.poster}" alt= "The Giant Gila Monster">
+    //     ${data.runtime}
+    //     ${data.showtime}
+    //     available tickets
+    //     `
+
+    // firstMovieCont.appendChild (firstMovie)
+
+    //     console.log (firstMovieCont)
+    // })
+
+//
+
+//
+ 
+            
+                // moviesHead.addEventListener('click', () => {
+                //     displayMovieDetails(films)
+                // })
+                
+// moviesHead.addEventListener('click', () => {
+//     displayMovieDetails(films)
+//      })
+
+//displayMovieDetails(films)
+
+// movieTitle.addEventListener('click', ()
+
+// function movieWhenClicked (moviesDetails, films) {
+//     moviesDetails.addEventListener ('click', ()=> {
+//         displayMovieDetails(films)
+//     })
+// }
+movieWhenClicked(moviesDetails, films)
+            displayMovieDetails(films)
+ moviesDetails.addEventListener('click', ()=>{
+     displayMovieDetails(films)
+ })
+ function displayMovieDetails(films) {
+    movies.innerHTML = ''
+
+    const moviesPoster = document.createElement('movieposter')
+    moviesPoster.src = films.poster
+    movies.appendChild(moviesPoster)
+
+    const moviesHead = document.createElement('p')
+    moviesHead.innerText = films.title
+    movies.appendChild(moviesHead)
+
+    const moviesRuntime = document.createElement('p')
+    moviesRuntime.innerText = films.runtime
+    movies.appendChild(moviesRuntime)
+
+    const moviesShowtime = document.createDocumentFragment('p')
+    moviesShowtime.innerText = films.showtime
+    movies.appendChild(moviesShowtime)
+
+    const availableTicketsButton = document.createElement('button')
+    availableTicketsButton.id = 'availableticketss'
+    availableTicketsButton.innerText = 'availableetickets'
+    movies.appendChild(availableTicketsButton)
+
+    moviesHead.addEventListener('click', () => {
+        displayMovieDetails(films)
+         })     
+ }
+displayMovieDetails(films)
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,32 @@
+# WELCOME TO Flatdango
+**Your one stop shop where you can view the latest movies showing at Flatiron Movie Theater**
+
+This repository focuses on Flatdango, an app where you can buy movie tickets for movies showing at Flatiron Movie Theater! I used a local API to build out the frontend for the app.
+The list of the movies currently showing is displayed in the homepage.
+When a user clicks on a movies’s name, they're able to see its details that is the movie poster, time of showing, the runtime of the movie and the number of tickets available for purchase. This prevents a customer from purchasing a ticket when they're sold out.
+
+
+**Setup Requirements**
+
+1. Git
+2. Github
+
+**Technologies Used**
+
+1. HTML
+2. CSS
+3. JavaScript
+
+**Author**
+
+Frida Nduta Gathima
+
+**Licence**
+
+Copyright (c) 2022 Frida Nduta Gathima.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files , to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
