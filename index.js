627aa4c676b560ca50e3c2dd28c5511ce388ac63
+ <+>
2023-07-03 16:16:27 +0300

    wk3 code challenge JS

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
 
-const movies = document.getElementById('films')
-fetch("http://localhost:3000/films")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(films => {
-            console.log(films)
+const movielistcontainer = document.getElementById('films') 
+const firstMovieCont = document.getElementById('firstmoviecontainer')
+const movieitemdetails =  document.getElementById('singlemovdiv')
 
-            const moviesHeading = document.createElement('p')
-            moviesHeading.innerText = films.title
-            movies.appendChild(moviesHeading)
-
-            const moviesImage = document.createElement('p')
-            moviesImage.innerHTML = `<img src = "${films.poster}" alt= "The Giant Gila Monster">`
-            movies.appendChild(moviesImage)
-
-            const moviesRuntime = document.createElement('p')
-            moviesRuntime.innerText = films.runtime
-            movies.appendChild(moviesRuntime)
-
-            const moviesShowtime = document.createElement('p')
-            moviesShowtime.innerText = films.showtime
-            movies.appendChild(moviesShowtime)
-
-            const availableTicketsButton = document.createElement('button')
-            availableTicketsButton.id = 'availableticketss'
-            availableTicketsButton.innerText = 'available tickets'
-            movies.appendChild(availableTicketsButton)
-            // availableTicketsButton.addEventListener('click', ()=>{
-                
-            // } )
-
-        }
-        )
-    })
 
 
 function displayFirstMovie() {
@@ -41,8 +11,7 @@ function displayFirstMovie() {
         .then((data) => {
             console.log(data)
 
-            const firstMovieCont = document.getElementById('firstmoviecontainer')
-
+            
             const movieHead = document.querySelector('h4')
             movieHead.innerText = data.title
             firstMovieCont.appendChild(movieHead)
@@ -50,141 +19,71 @@ function displayFirstMovie() {
             const moviePoster = document.getElementById('poster')
             moviePoster.innerHTML = `<img src = "${data.poster}" alt= "The Giant Gila Monster">`
             firstMovieCont.appendChild(moviePoster)
+           
+           const runtimetext = document.createElement('p')
+           runtimetext.innerText = "Runtime   "+ ": " +data.runtime
+           firstMovieCont.append(runtimetext)
 
-            const movieRuntime = document.getElementById('runtime')
-            movieRuntime.innerText = 'The runtime for this movie is ' + '' + data.runtime + '' + ' minutes'
-            firstMovieCont.appendChild(movieRuntime)
-
-            const movieShowtime = document.getElementById('showtime')
-            movieShowtime.innerText = 'This movie starts at ' + '' + data.showtime
-            firstMovieCont.appendChild(movieShowtime)
-
-            const ticketbutton = document.createElement('button')
-            ticketbutton.id = 'availableticketss'
-            ticketbutton.innerText = 'AVAILABLE TICKETS'
-            firstMovieCont.appendChild(ticketbutton)
-
-            const tickets = document.getElementById('availabletickets')
-            tickets.innerText = 'available_tickets'
-            firstMovieCont.appendChild(tickets)
-
-            const ticketting = document.getElementById('availabletickets')
-            ticketting.addEventListener('click', availableTickets(ticketssold, theatrecapacity))
-
-
-            // tickets.addEventListener('click', function availableTickets(ticketssold, theatrecapacity) {
-            //     if (ticketssold <= theatrecapacity) {
-            //         if (ticketssold === theatrecapacity) {
-            //             return 'no available tickets'
-            //         } else {
-            //             if (ticketssold < theatrecapacity) {
-            //                 return (theatrecapacity - ticketssold)
-            //             }
-            //         }
-            //     }
-            // })
-
-            //tickets.innerText = availableTickets (ticketssold, theatrecapacity)
-            //const availabletickets = availableTickets (ticketssold, theatrecapacity)
-
-
+           const availabletiks = document.createElement('p')
+           availabletiks.innerText = "Availabletickets " + "= " + (data.capacity - data.tickets_sold) 
+           firstMovieCont.append(availabletiks)
+       
         })
 }
 displayFirstMovie()
 
-function availableTickets(ticketssold, theatrecapacity) {
-    if (ticketssold <= theatrecapacity) {
-        if (ticketssold === theatrecapacity) {
-            return 'no available tickets'
-        } else {
-            if (ticketssold < theatrecapacity) {
-                return (theatrecapacity - ticketssold)
-            }
-        }
-    }
+function showallmovielist(){
+   fetch ("http://localhost:3000/films")
+   .then (response => response.json())
+   .then((data) => {
+        console.log(data)
+        data.forEach(dataitem => {
+            
+            const movielistitem = document.createElement('li')
+            movielistitem.innerText = dataitem .title
+            movielistcontainer.append(movielistitem)
+           
+            movielistitem.addEventListener('click' , ()=>{
+                displayMovieItemFun(dataitem)
+            })
+
+        });
+   })
 }
-console.log(availableTickets(1, 3))
+showallmovielist()
+function displayMovieItemFun(dataitem) {
+    movieitemdetails.innerHTML = ""
+
+    const movieitemtitle = document.createElement('p')
+    movieitemtitle.innerText = dataitem.title
+    movieitemdetails.append(movieitemtitle)
+
+    const movieitemposter = document.createElement('img')
+    movieitemposter.src = dataitem.poster
+    movieitemdetails.append(movieitemposter)
+    
+    const movieitemruntime = document.createElement('p')
+    movieitemruntime.innerText = "Runtime: " + dataitem.runtime
+    movieitemdetails.append(movieitemruntime)
+    
+    const movieitemshowtime = document.createElement('p')
+    movieitemshowtime.innerText = "Showtime: " + dataitem.showtime
+    movieitemdetails.append(movieitemshowtime)
+    
+    const movieitemavailabletic = document.createElement('p')
+    movieitemavailabletic.id = 'availabletickss'
+    movieitemavailabletic.innerText = "Available tickets " + "= " + (dataitem.capacity - dataitem.tickets_sold)
+    movieitemdetails.append(movieitemavailabletic)
+    
+    const availableticsButton = document.createElement('button')
+    availableticsButton.innerText = "Buy ticket"
+
+    movieitemdetails.append(availableticsButton)
+    availableticsButton.addEventListener('click' , ()=>{
+        
+        const availabletiks = document.getElementById('availabletickss')
+        availabletiks.innerText = "Available tickets = " + (dataitem.capacity)--
 
 
-
-
-
-// movieHead.addEventListener ('click', ()=> {
-//     availableTickets(ticketssold, theatrecapacity)      
-// })
-
-
-//     const firstMovie = document.createElement('div')
-//     firstMovie.innerHTML = `
-//     ${data.title}
-//     <img src = "${data.poster}" alt= "The Giant Gila Monster">
-//     ${data.runtime}
-//     ${data.showtime}
-//     available tickets
-//     `
-
-// firstMovieCont.appendChild (firstMovie)
-
-//     console.log (firstMovieCont)
-// })
-
-//
-
-//
-
-
-// moviesHead.addEventListener('click', () => {
-//     displayMovieDetails(films)
-// })
-
-// moviesHead.addEventListener('click', () => {
-//     displayMovieDetails(films)
-//      })
-
-//displayMovieDetails(films)
-
-// movieTitle.addEventListener('click', ()
-
-// function movieWhenClicked (moviesDetails, films) {
-//     moviesDetails.addEventListener ('click', ()=> {
-//         displayMovieDetails(films)
-//     })
-// }
-// movieWhenClicked(moviesDetails, films)
-// displayMovieDetails(films)
-// moviesDetails.addEventListener('click', () => {
-//     displayMovieDetails(films)
-// })
-// // function displayMovieDetails(films) {
-//     movies.innerHTML = ''
-
-//     const moviesPoster = document.createElement('movieposter')
-//     moviesPoster.src = films.poster
-//     movies.appendChild(moviesPoster)
-
-//     const moviesHead = document.createElement('p')
-//     moviesHead.innerText = films.title
-//     movies.appendChild(moviesHead)
-
-//     const moviesRuntime = document.createElement('p')
-//     moviesRuntime.innerText = films.runtime
-//     movies.appendChild(moviesRuntime)
-
-//     const moviesShowtime = document.createElement('p')
-//     moviesShowtime.innerText = films.showtime
-//     movies.appendChild(moviesShowtime)
-
-//     const availableTicketsButton = document.createElement('button')
-//     availableTicketsButton.id = 'availableticketss'
-//     availableTicketsButton.innerText = 'availableetickets'
-//     movies.appendChild(availableTicketsButton)
-
-//     moviesHead.addEventListener('click', () => {
-//         displayMovieDetails(films)
-//     })
-// }
-// displayMovieDetails(films)
-
-
-
-
+    })
+}
